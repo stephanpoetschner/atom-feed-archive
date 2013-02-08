d847e5248bc17a7804a8d14636d56c12fedc43dd
Stephan Poetschner <stephan.poetschner@gmail.com>
2013-02-08 09:42:57 +0100

    + fixing missing import

--- convert_atom.py
+++ convert_atom.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
 # coding: utf-8
 from BeautifulSoup import BeautifulSoup
 import HTMLParser
 from os.path import basename
 
 from django.template.defaultfilters import slugify
+import requests
 
 parser = HTMLParser.HTMLParser()
 soup = BeautifulSoup(open('feed.atom'))
